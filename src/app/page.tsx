701e61036cd910b5de5545d9f7160026389f0fa0
Matheus Carvalho <matheusdocarvalho@gmail.com>
2025-08-05 22:17:34 -0400

    feat: add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
+import { desc } from "drizzle-orm";
 import Image from "next/image";
 
 import CategorySelector from "@/components/common/category-selector";
+import Footer from "@/components/common/footer";
 import Header from "@/components/common/header";
 import ProductList from "@/components/common/products-list";
 import { db } from "@/db";
+import { productTable } from "@/db/schema";
 
 export default async function Home() {
   const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
+  const newlyCreatedProducts = await db.query.productTable.findMany({
+    orderBy: [desc(productTable.createdAt)],
     with: {
       variants: true,
     },
@@ -45,6 +54,8 @@ export default async function Home() {
             className="h-auto w-full"
           />
         </div>
+        <ProductList title="Novos produtos" products={newlyCreatedProducts} />
+        <Footer />
       </div>
     </>
   );
